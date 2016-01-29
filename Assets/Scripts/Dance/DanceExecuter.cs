0636d0ba39abe4427a8b8ae366a5dd047854153b
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-01-29 21:41:46 +0000

    Basic player movement

---
+++ Assets/Scripts/Dance/DanceExecuter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DanceExecuter : MonoBehaviour {
+
+    [SerializeField] private float _beatLength = 1;
+    [SerializeField] private PlayerControl[] _players;
+    private float _danceTime;
+
+    protected void Start() {
+        int i = 0;
+        foreach (var p in _players) {
+            p.Init(this, i++);
+        }
+    }
+
+    protected void Update() {
+        _danceTime += Time.deltaTime;
+        if (_danceTime >= _beatLength) {
+            _danceTime -= _beatLength;
+            Tick();
+        }
+    }
+
+    private void Tick() {
+        int i = 0;
+        var moves = new PlayerControl.PlayerMoves[2];
+        foreach (var p in _players) {
+            p.OnDanceTick();
+            moves[i++] = p.GetMove();
+        }
+        ProcessMoves(moves);
+    }
+
+    private void ProcessMoves(PlayerControl.PlayerMoves[] moves) {
+        Debug.Log(moves[0] + ", " + moves[1]);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    private Vector2 _lastInput;
+    [SerializeField] private int _playerNumber = 0;
+    [SerializeField] private int _joystickNumber = 0;
+    [SerializeField] private int _stickNumber = 0;
+    private DanceExecuter _dance;
+    private PlayerMoves _myMove;
+
+    public enum PlayerMoves {
+        Idle,
+        Left,
+        Right,
+        Up,
+        Down
+    }
+    
+    protected void Update () {
+	    SetInput(GetInputStick());
+	}
+
+    public void Init(DanceExecuter dance, int playerNumber) {
+        _dance = dance;
+        _playerNumber = playerNumber;
+        if (playerNumber == 1) {
+            _stickNumber = 1;
+        }
+    }
+
+    private void SetInput(Vector2 input) {
+        _lastInput = input;
+    }
+
+    private Vector2 GetInputStick() {
+        var joy = "Joy" + _joystickNumber + " " + (_stickNumber == 0 ? "Left" : "Right");
+        var x = joy + "X";
+        var y = joy + "Y";
+        return new Vector2(Input.GetAxisRaw(x), Input.GetAxisRaw(y));
+    }
+
+    public void OnDanceTick() {
+        var move = PlayerMoves.Idle;
+        if (_lastInput.magnitude > 0.5f) {
+            if (Mathf.Abs(_lastInput.x) > Mathf.Abs(_lastInput.y)) {
+                move = _lastInput.x > 0 ? PlayerMoves.Right : PlayerMoves.Left;
+            } else {
+                move = _lastInput.y > 0 ? PlayerMoves.Down : PlayerMoves.Up;
+            }
+        }
+        DoMove(move);
+    }
+
+    private void DoMove(PlayerMoves move) {
+        switch (move) {
+        case PlayerMoves.Down:
+            StartCoroutine(Jump(0, 0));
+            break;
+        case PlayerMoves.Up:
+            StartCoroutine(Jump(0, 1f));
+            break;
+        case PlayerMoves.Left:
+            StartCoroutine(Jump(-1, 0.5f));
+            break;
+        case PlayerMoves.Right:
+            StartCoroutine(Jump(1, 0.5f));
+            break;
+        default:
+            break;
+        }
+        _myMove = move;
+    }
+
+    private IEnumerator Jump(float deltaX, float height) {
+        Vector2 startPos = transform.position;
+        float length = 0.4f;
+        float factor = 1 / length;
+        for (float t = 0; t < 1; t += Time.deltaTime * factor) {
+            transform.position = startPos + new Vector2(deltaX * t, height * 4 * t * (1 - t));
+            yield return null;
+        }
+        transform.position = startPos + new Vector2(deltaX, 0);
+    }
+
+    public PlayerMoves GetMove() {
+        return _myMove;
+    }
+}
